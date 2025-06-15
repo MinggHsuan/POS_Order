5fef0c89f02d7326a6a06985dfac524aa6daa136
DESKTOP-7PA0E13\user <d092946593@gmail.com>
2025-06-15 22:55:41 +0800

    json載入完成

--- POS_Order/Form1.cs
+++ POS_Order/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -23,29 +26,36 @@ namespace POS_Order
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            string menupath = ConfigurationManager.AppSettings["MenuPath"];
+            string menujson = File.ReadAllText(menupath, Encoding.UTF8);
+            MenuModel menuModel = JsonConvert.DeserializeObject<MenuModel>(menujson);
+
+
             string[] mainFoods = { "雞腿飯$90", "雞排飯$85", "排骨飯$70" };
             string[] sideFoods = { "薯條$40", "雞塊$50", "薯球$20" };
             string[] dessert = { "草莓蛋糕$100", "巧克力冰$95", "聖代$200" };
             string[] drinks = { "紅茶$20", "綠茶$30", "奶茶$40" };
 
-            Dictionary<string, string> dict = new Dictionary<string, string>
-            {
-                { "雞腿飯買二送一", "POS_Order.Discounts.Chicken_BuyTwoOneFree" },
-                { "雞排飯買三個打85折", "POS_Order.Discounts.Chicken_15off" },
-                { "排骨飯搭配薯條100元", "POS_Order.Discounts.RibsWithFries_100" },
-                { "雞排飯搭配聖代送紅茶一杯", "POS_Order.Discounts.ChickenWithSundae_BlackTea" },
-                { "雞排飯搭配薯條打95折", "POS_Order.Discounts.ChickenWithFries_5off" },
-                { "飲料均一價20元", "POS_Order.Discounts.AllDrink_20NT" },
-                { "所有飲料買三杯就送一杯(送最便宜品項)", "POS_Order.Discounts.AllDrink_BuyThreeOneFree" },
-                { "排骨飯搭配任一種配餐就送奶茶", "POS_Order.Discounts.RibsWithSide_MilkTea" },
-                { "全場消費滿500折100", "POS_Order.Discounts.AllItemOver500_100" },
-                { "全場一律打9折", "POS_Order.Discounts.AllItem_10off" }
-            };
+
             flowLayoutPanel1.AutoGenerate(mainFoods, Checkbox_CheckedChange, Numberic_ValueChange);
             flowLayoutPanel2.AutoGenerate(sideFoods, Checkbox_CheckedChange, Numberic_ValueChange);
             flowLayoutPanel3.AutoGenerate(dessert, Checkbox_CheckedChange, Numberic_ValueChange);
             flowLayoutPanel4.AutoGenerate(drinks, Checkbox_CheckedChange, Numberic_ValueChange);
             PanelHandlers.Handler += GetPanel;
+
+            Dictionary<string, string> dict = new Dictionary<string, string>
+              {
+                  { "雞腿飯買二送一", "POS_Order.Discounts.Chicken_BuyTwoOneFree" },
+                  { "雞排飯買三個打85折", "POS_Order.Discounts.Chicken_15off" },
+                  { "排骨飯搭配薯條100元", "POS_Order.Discounts.RibsWithFries_100" },
+                  { "雞排飯搭配聖代送紅茶一杯", "POS_Order.Discounts.ChickenWithSundae_BlackTea" },
+                  { "雞排飯搭配薯條打95折", "POS_Order.Discounts.ChickenWithFries_5off" },
+                  { "飲料均一價20元", "POS_Order.Discounts.AllDrink_20NT" },
+                  { "所有飲料買三杯就送一杯(送最便宜品項)", "POS_Order.Discounts.AllDrink_BuyThreeOneFree" },
+                  { "排骨飯搭配任一種配餐就送奶茶", "POS_Order.Discounts.RibsWithSide_MilkTea" },
+                  { "全場消費滿500折100", "POS_Order.Discounts.AllItemOver500_100" },
+                  { "全場一律打9折", "POS_Order.Discounts.AllItem_10off" }
+              };
             List<DiscountModel> discountModels = new List<DiscountModel>();
             foreach (var item in dict)
             {
---
+++ POS_Order/MenuModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Order
+{
+    internal class MenuModel
+    {
+        public Menu[] Menus { get; set; }
+        public Discount[] Discounts { get; set; }
+
+        public class Menu
+        {
+            public string Title { get; set; }
+            public Food[] Foods { get; set; }
+        }
+
+        public class Food
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+        public class Discount
+        {
+            public string Name { get; set; }
+            public string Strategy { get; set; }
+            public Condition[] Conditions { get; set; }
+            public Reward[] Rewards { get; set; }
+        }
+
+        public class Condition
+        {
+            public string Name { get; set; }
+            public int RequirAmount { get; set; }
+            public int RequirPrice { get; set; }
+        }
+
+        public class Reward
+        {
+            public string Name { get; set; }
+            public object RewardType { get; set; }
+            public int RewardsAmount { get; set; }
+            public int RewardsPrice { get; set; }
+            public float RewardsOff { get; set; }
+        }
+
+    }
+}
